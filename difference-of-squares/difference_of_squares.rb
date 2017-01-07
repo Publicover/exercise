15278768c8ef7201845368c84f19807518772359
Jim Publicover <jameslpublicover@gmail.com>
2017-01-07 13:35:33 -0500

    difference_of_squares complete

---
+++ difference-of-squares/difference_of_squares.rb
@@ -0,0 +1,29 @@
+class Squares
+
+  def initialize(number)
+    @number = number
+  end
+
+  def square_of_sum
+    square_array = (1..@number).to_a
+    square_sum = 0
+    square_array.each do |num|
+      square_sum += num
+    end
+    square_sum ** 2
+  end
+
+  def sum_of_squares
+    square_array = (1..@number).to_a
+    square_sum = 0
+    square_array.each do |num|
+      square_sum += num * num
+    end
+    square_sum
+  end
+
+  def difference
+    square_of_sum - sum_of_squares
+  end
+
+end
