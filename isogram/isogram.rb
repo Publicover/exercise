ffdc07d01de4f6c1c4994e7e2c29a97188da61c3
Jim Publicover <jameslpublicover@gmail.com>
2017-01-13 16:00:09 -0500

    isogram complete

---
+++ isogram/isogram.rb
@@ -0,0 +1,16 @@
+class Isogram
+
+  def self.is_isogram?(string)
+
+    input_letters = (string.downcase.gsub(/\s+/, "").gsub(/\-/, "")).split("")
+    unique_letters = input_letters.uniq
+
+    if input_letters.size == unique_letters.size
+      true
+    else
+      false
+    end
+
+  end
+
+end
