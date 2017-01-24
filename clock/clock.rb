7e4a582feaedef962b0ea2b89290808cdd51771a
Jim Publicover <jameslpublicover@gmail.com>
2017-01-24 10:55:57 -0500

    add another if branch. ugly but working.

--- clock/clock.rb
+++ clock/clock.rb
@@ -6,7 +6,7 @@ class Clock
     elsif hour < 24 && minute < 10
       "#{hour}:0#{minute}"
     elsif hour >= 24 && minute < 10
-      if hour < 10 && minute < 10
+      if hour % 24 < 10 && minute < 10
         "0#{hour % 24}:0#{minute}"
       else
         "#{hour % 24}:0#{minute}"
