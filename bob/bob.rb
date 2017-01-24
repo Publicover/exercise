38d07c1513c2061a883016a8e48bb5385d77493f
Jim Publicover <jameslpublicover@gmail.com>
2017-01-23 21:01:41 -0500

    bob up and running

---
+++ bob/bob.rb
@@ -0,0 +1,16 @@
+class Bob
+
+  def self.hey(remark)
+    remark.gsub!(/\n/, "")
+    if remark == remark.upcase && remark.downcase != remark
+      'Whoa, chill out!'
+    elsif remark[-1] == "?"
+      'Sure.'
+    elsif remark =~ /^\s*$/
+      'Fine. Be that way!'
+    else
+      'Whatever.'
+    end
+  end
+
+end
