03843f6e5caf9b22d9198b6e8879352f268ccedf
Jim Publicover <jameslpublicover@gmail.com>
2017-01-29 13:22:03 -0500

    clean out comments that aren't working

--- phone-number/phone_number.rb
+++ phone-number/phone_number.rb
@@ -3,26 +3,11 @@ class PhoneNumber
   attr_reader :number
 
   def initialize(phone_number)
-
-    if phone_number =~ /abc/ || phone_number.length < 10
+    if phone_number =~ /abc/ || phone_number.length <= 10
       @number = "0000000000"
     else
-
       @number = phone_number.gsub!(/[^\d]/, "")
-
     end
   end
-
-    # replacement_rules = {
-    #   " " => "",
-    #   "(" => "",
-    #   ")" => "",
-    #   "-" => ""
-    # }
-    #
-    # matcher = /#{replacement_rules.keys.join('|')}/
-    #
-    # @number = phone_number.gsub(matcher, replacement_rules)
-
 
 end
