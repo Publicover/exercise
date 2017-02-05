5ce1bf42311f8684060392f01dbd09365d1254a1
Jim Publicover <jameslpublicover@gmail.com>
2017-02-05 15:52:44 -0500

    triangle complete

---
+++ triangle/triangle.rb
@@ -0,0 +1,56 @@
+class Triangle
+
+  attr_reader :sides
+
+  def initialize(sides)
+    @sides = sides
+  end
+
+
+  def equilateral?
+    if sides.include?(0)
+      return false
+    else
+      if sides[0] == sides[1] && sides[1] == sides[2]
+        true
+      else
+        false
+      end
+    end
+  end
+
+  def isosceles?
+    unless violates_equal?
+      if sides.include?(0)
+        return false
+      else
+        if sides[0] == sides[1] || sides[1] == sides[2] || sides[0] == sides[2]
+          true
+        else
+          false
+        end
+      end
+    end
+  end
+
+  def scalene?
+    unless violates_equal?
+      if sides[0] != sides[1] && sides[1] != sides[2] && sides[0] != sides[2]
+        true
+      else
+        false
+      end
+    end
+  end
+
+  private
+
+    def violates_equal?
+      if sides[0] + sides[1] <= sides[2] || sides[1] + sides[2] <= sides[0] || sides[0] + sides[2] <= sides[1]
+        true
+      else
+        false
+      end
+    end
+
+end
