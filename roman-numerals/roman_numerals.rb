39086c6b1dbfb874db8075fd493de47f13e39160
Jim Publicover <jameslpublicover@gmail.com>
2017-02-06 20:25:30 -0500

    v2, times do takes a block

--- roman-numerals/roman_numerals.rb
+++ roman-numerals/roman_numerals.rb
@@ -6,82 +6,43 @@ class Fixnum
     x = self
     y = ""
 
-    (x / 1_000).times do
-      y << "M"
-    end
-
+    (x / 1_000).times { y << "M" }
     x = (x % 1_000)
 
-    (x / 900).times do
-      y << "CM"
-    end
-
+    (x / 900).times { y << "CM" }
     x = (x % 900)
 
-    (x / 500).times do
-      y << "D"
-    end
-
+    (x / 500).times { y << "D" }
     x = (x % 500)
 
-    (x / 400).times do
-      y << "CD"
-    end
-
+    (x / 400).times { y << "CD" }
     x = (x % 400)
 
-    (x / 100).times do
-      y << "C"
-    end
-
+    (x / 100).times { y << "C" }
     x = (x % 100)
 
-    (x / 90).times do
-      y << "XC"
-    end
-
+    (x / 90).times { y << "XC" }
     x = (x % 90)
 
-    (x / 50).times do
-      y << "L"
-    end
-
+    (x / 50).times { y << "L" }
     x = (x % 50)
 
-    (x / 40).times do
-      y << "XL"
-    end
-
+    (x / 40).times { y << "XL" }
     x = (x % 40)
 
-    (x / 10).times do
-      y << "X"
-    end
-
+    (x / 10).times { y << "X" }
     x = (x % 10)
 
-    (x / 9).times do
-      y << "IX"
-    end
-
+    (x / 9).times { y << "IX" }
     x = (x % 9)
 
-    (x / 5).times do
-      y << "V"
-    end
-
+    (x / 5).times { y << "V"}
     x = (x % 5)
 
-    (x / 4).times do
-      y << "IV"
-    end
-
+    (x / 4).times { y << "IV" }
     x = (x % 4)
 
-    (x / 1).times do
-      y << "I"
-    end
-
+    (x / 1).times { y << "I" }
     y
   end
 
