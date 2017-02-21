c9f25a642c1192a8b434242c8e0847d97d9a514b
Jim Publicover <jameslpublicover@gmail.com>
2017-02-21 13:19:05 -0500

    scrabble score up and running

--- clock/clock.rb
+++ clock/clock.rb
@@ -2,31 +2,45 @@ class Clock
 
   def self.at(hour, minute)
 
-    # hour = hour % 24
-    # extra_hour = minute / 60
-    # hour += extra_hour
-    # extra_minute = minute / 60
+    # if hour < 10 && minute < 10
+    #   "0#{hour}:0#{minute}"
+    # elsif hour < 24 && minute < 10
+    #   "#{hour}:0#{minute}"
+    # elsif hour < 24 && minute >=10
+    #   "0#{hour}:#{minute}"
+    # elsif hour >= 24 && minute < 10
+    #   if hour % 24 < 10 && minute < 10
+    #     "0#{hour % 24}:0#{minute}"
+    #   else
+    #     "#{hour % 24}:0#{minute}"
+    #   end
+    # elsif hour >= 24 && minute >= 10
+    #   "#{hour}:#{minute}"
+    # else
+    #   "hold, please"
+    # end
+
     hour += (minute / 60)
+    hour = hour % 24
     minute = minute % 60
+
+    if hour < 0
+      hour = 24 - (hour % 24)
+    end
 
     if hour < 10 && minute < 10
       "0#{hour}:0#{minute}"
-    elsif hour < 24 && minute < 10
-      "#{hour}:0#{minute}"
-    elsif hour < 24 && minute >=10
+    elsif hour < 10 && minute >= 10
       "0#{hour}:#{minute}"
-    elsif hour >= 24 && minute < 10
-      if hour % 24 < 10 && minute < 10
-        "0#{hour % 24}:0#{minute}"
-      else
-        "#{hour % 24}:0#{minute}"
-      end
-    # elsif hour >= 24 && minute >= 10
-    #   "#{hour}:#{minute}"
+    elsif hour >= 10 && minute < 10
+      "#{hour}:0#{minute}"
     else
-      "hold, please"
+      "#{hour}:#{minute}"
     end
+  end
 
+  def +(extra_minute)
+    minute += extra_minute
   end
 
 end
