51ba9f25f15044abafdd07ba953713476b49472d
Jim Publicover <jameslpublicover@gmail.com>
2017-03-17 18:41:49 -0400

    new way to think about it, reduce time

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -4,31 +4,46 @@ class Phrase
     @phrase = phrase
   end
 
+  # def word_count
+  #
+  #   phrase_hash = {}
+  #   phrase_array = @phrase.downcase.split
+  #
+  #   # phrase_array.each { |word| phrase_hash[word] = 0 }
+  #   # phrase_hash.each_key do |word|
+  #   #   # if phrase_hash[word] > 1
+  #   #     phrase_hash[word] += 1
+  #   #   # end
+  #   # end
+  #   # phrase_hash
+  #
+  #   phrase_array.each do |word|
+  #     word.tr!(".,:!&@$%^&", "")
+  #     if word[0] && word[-1] == "'"
+  #       word.gsub!("''", "")
+  #     end
+  #     if phrase_hash.include?(word)
+  #       phrase_hash[word] += 1
+  #     else
+  #       phrase_hash[word] = 1
+  #     end
+  #   end
+  #   phrase_hash
+  #
+  #   # phrase_array
+  # end
+
   def word_count
 
-    phrase_hash = {}
-    # phrase_array = @phrase.split(/[^\w]/)
-    phrase_array = @phrase.downcase.split(/[^\w]\b/)
-
-    # phrase_array.each { |word| phrase_hash[word] = 0 }
-    # phrase_hash.each_key do |word|
-    #   # if phrase_hash[word] > 1
-    #     phrase_hash[word] += 1
-    #   # end
-    # end
-    # phrase_hash
+    phrase_array = @phrase.downcase.gsub(/[.,:!&@$%^&]/, " ").split
+    phrase_hash = Hash.new(0)
 
     phrase_array.each do |word|
-      word.tr!(",:!&@$%^&", "")
-      if phrase_hash.include?(word)
-        phrase_hash[word] += 1
-      else
-        phrase_hash[word] = 1
-      end
+      phrase_hash[word] += 1
     end
+
     phrase_hash
 
-    # phrase_array
   end
 
 end
