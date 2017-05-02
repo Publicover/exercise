8a00b217a5828727f9aa797dcfab2eb1a86eee58
Jim Publicover <jameslpublicover@gmail.com>
2017-05-02 17:51:49 -0400

    passing all tests in atbash-cipher

--- grains/grains.rb
+++ grains/grains.rb
@@ -15,13 +15,7 @@ class Grains
   end
 
   def self.total
-    # full_total = 2
     total_squares = 64
-    # (total_squares - 2).times do
-    #   full_count *= 2
-    # end
-    # full_count
-    # 18_446_744_073_709_551_615
     (1..total_squares).inject(0) {|sum, x| sum + square(x)}
   end
 
