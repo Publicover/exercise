0903133920fcddbcb5e4e49c0011f8602208b553
Jim Publicover <jameslpublicover@gmail.com>
2017-05-06 09:45:26 -0400

    need to use scan to seperate string

---
+++ crypto-square/crypto_square.rb
@@ -0,0 +1,19 @@
+class Crypto
+
+  def initialize(plaintext)
+    @plaintext = plaintext
+  end
+
+  def normalize_plaintext
+    @plaintext.downcase.gsub(/\W/, "")
+  end
+
+  def size
+    (normalize_plaintext.length**0.5).ceil
+  end
+
+  def plaintext_segments
+    # normalize_plaintext.scan(/.{5}|.+/).join(" ")
+  end
+
+end
