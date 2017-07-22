30e2e841e8c427a3c9998157daf128e91ccd8d54
Jim Publicover <jameslpublicover@gmail.com>
2017-07-21 21:18:25 -0400

    adding initialize gets histogram method to pass

--- nucleotide-count/nucleotide_count.rb
+++ nucleotide-count/nucleotide_count.rb
@@ -1,14 +1,18 @@
 class Nucleotide
 
-  def self.from_dna(strand)
+  def initialize(strand)
     @strand = strand
+  end
+
+  def self.from_dna(strand)
+    Nucleotide.new(strand)
   end
 
   def count(nucleotide)
     # @strand = 0
     # @nucleotide = nucleotide
     total = 0
-    @strand.split("").each do |letter|
+    @strand.chars.each do |letter|
       if letter == nucleotide
         total += 1
       end
@@ -17,13 +21,35 @@ class Nucleotide
   end
 
   def histogram
-  #   final_histogram = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
-  #   @strand.split("").each do |letter|
-  #     if letter == final_histogram[letter]
-  #       final_histogram[letter] += 1
-  #     end
-  #   end
-  #   final_histogram
+    final_histogram = { 'A' => 0, 'T' => 0, 'C' => 0, 'G' => 0 }
+    @strand.chars.each do |letter|
+      if letter == final_histogram[letter]
+        final_histogram[letter] += 1
+      end
+    end
+    final_histogram
   end
 
 end
+
+
+# class Nucleotide
+#   def self.from_dna(strand)
+#     Nucleotide.new(strand)
+#   end
+#
+#   def initialize(strand)
+#     fail ArgumentError if strand.match(/[^ATCG]/)
+#     @strand = strand
+#   end
+#
+#   def count(nucleotide)
+#     @strand.chars.count { |c| c == nucleotide }
+#   end
+#
+#   def histogram
+#     %w(A T C G).each_with_object({}) do |c, hist|
+#       hist[c] = count(c)
+#     end
+#   end
+# end
