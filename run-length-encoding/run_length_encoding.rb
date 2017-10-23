7904dea09e33827dad1ab22499dfe1bf254fb490
Jim Publicover <jameslpublicover@gmail.com>
2017-10-23 15:13:28 -0400

    clean up run-length-encoding

--- run-length-encoding/run_length_encoding.rb
+++ run-length-encoding/run_length_encoding.rb
@@ -23,59 +23,32 @@ class RunLengthEncoding
   end
 
   def self.decode(input)
-    # if it doesn't have any numbers, just dump into string and return that
     if /^([^0-9]*)$/ =~ input
       return input
-    # if the input contains numbers, do some stuff
     else
-      # split the inital string into an array
       raw_letter_data = input.split("")
-      # initialize the final string
       decoded_data = ""
 
-      # take the input array and the elements' indecies for iteration
       raw_letter_data.each_with_index do |letter, i|
-        # if the element is a digit and the following element is also a digit
         if /\d/ =~ letter && /\d/ =~ raw_letter_data[i + 1]
-          # use those digits to create a number of times to push the element to the final string
           ("#{letter}#{raw_letter_data[i + 1]}".to_i).times do |number|
-            # push to string
             decoded_data << raw_letter_data[i + 2]
           end
-          # if the element is not a digit and the previous element is a digit
-        elsif /\D/ =~ letter && /\d/ =~ raw_letter_data[i - 1]
-          next
-        # if the element is a digit and neither following nor previous elements are digits
-        elsif /\d/ =~ letter && /\D/ =~ raw_letter_data[i + 1] && /\D/ =~ raw_letter_data[i - 1]
-          # use the single digit to push to the string that number of times
-          letter.to_i.times do |number|
-            # push to string
-            decoded_data << raw_letter_data[i + 1]
-          end
-        # # if the element is not a digit and both previous and next elements are not digits
-        # elsif /\D/ =~ letter && /\D/ =~ raw_letter_data[i + 1] && /\D/ =~ raw_letter_data[i - 1]
-        #   # push single letter to string
-        #   decoded_data << letter
-
-        #if the element is not a digit and what comes after is not digit
+      elsif /\D/ =~ letter && /\d/ =~ raw_letter_data[i - 1]
+        next
+      elsif /\d/ =~ letter && /\D/ =~ raw_letter_data[i + 1] && /\D/ =~ raw_letter_data[i - 1]
+        letter.to_i.times do |number|
+          decoded_data << raw_letter_data[i + 1]
+        end
       elsif /\D/ =~ letter && /\D/ =~ raw_letter_data[i + 1]
-          # push single letter to string
           decoded_data << letter
-        #if the element is not a digit and what comes before is not a digit and what comes after is a digit
-        elsif /\D/ =~ letter && /\D/ =~ raw_letter_data[i - 1] && /\d/ =~ raw_letter_data[i + 1]
-          decoded_data << letter
-        elsif /\D/ =~ letter && letter == raw_letter_data[-1]
-          decoded_data << letter
-
-
-
+      elsif /\D/ =~ letter && /\D/ =~ raw_letter_data[i - 1] && /\d/ =~ raw_letter_data[i + 1]
+        decoded_data << letter
+      elsif /\D/ =~ letter && letter == raw_letter_data[-1]
+        decoded_data << letter
         end
       end
-      # call the final string
       decoded_data
-
     end
-
   end
-
 end
