4c2878e629a7f65f90761d4d704796cef6d19b33
Jim Publicover <jameslpublicover@gmail.com>
2017-11-13 06:32:47 -0500

    refactoring

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,27 +1,24 @@
+# raindrops
 class Raindrops
-
   def self.convert(number)
-    number_string = ""
+    number_string = ''
 
-
-    if number % 3 == 0 && number % 5 == 0 && number % 7 == 0
-      number_string << "PlingPlangPlong"
-    elsif number % 3 == 0 && number % 5 == 0
-      number_string << "PlingPlang"
-    elsif number % 5 == 0 && number % 7 == 0
-      number_string << "PlangPlong"
-    elsif number % 3 == 0 && number % 7 == 0
-      number_string << "PlingPlong"
-    elsif number % 3 == 0
-      number_string << "Pling"
-    elsif number % 5 == 0
-      number_string << "Plang"
-    elsif number % 7 == 0
-      number_string << "Plong"
+    if (number % 3).zero? && (number % 5).zero? && (number % 7).zero?
+      number_string << 'PlingPlangPlong'
+    elsif (number % 3).zero? && (number % 5).zero?
+      number_string << 'PlingPlang'
+    elsif (number % 5).zero? && (number % 7).zero?
+      number_string << 'PlangPlong'
+    elsif (number % 3).zero? && (number % 7).zero?
+      number_string << 'PlingPlong'
+    elsif (number % 3).zero?
+      number_string << 'Pling'
+    elsif (number % 5).zero?
+      number_string << 'Plang'
+    elsif (number % 7).zero?
+      number_string << 'Plong'
     else
       number_string << number.to_s
     end
-
   end
-
 end
